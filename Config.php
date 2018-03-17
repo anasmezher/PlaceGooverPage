d3e5051897f0bbe61559da0fb4d6be47d58722dc
Anas Mezher <anasmezherr@gmail.com>
2018-03-17 19:15:07 +0300

    first commit to ms-branch

--- Config.php
+++ Config.php
@@ -5,5 +5,6 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'anas111');
 define('DB_DATABASE', 'placesnew');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+this code in ms-branch branch
 
 ?>
