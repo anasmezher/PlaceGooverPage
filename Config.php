469a929767ebf12f8c6df790b519f02caf65c083
Anas Mezher <anasmezherr@gmail.com>
2018-03-17 18:56:37 +0300

    my first add commit

--- Config.php
+++ Config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'anasmezher');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'anas111');
-define('DB_DATABASE', 'places');
+define('DB_DATABASE', 'placesnew');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
